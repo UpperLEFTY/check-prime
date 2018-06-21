3db73a723b145ac16e4f31fc4b9b27b17fb3a349
Essence Alexander <upperlefty@zoho.eu>
2018-06-21 01:15:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ function isPrime(n){
   = true
 > isPrime(237);
   = false
+
+
+// Second 
+
+
+function isPrime(n)
+{
+  var divisor = 3, 
+      limit = Math.sqrt(n);
+  
+  //check simple cases
+  if (n == 2 || n == 3)
+     return true;
+  if (n % 2 == 0)
+     return false;
+
+  while (divisor <= limit)
+  {
+    if (n % divisor == 0)
+      return false;
+    else
+      divisor += 2;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
